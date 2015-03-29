59a526f9212cd5340484da26c00de973c40e4eb6
Ariel <arieltian99@gmail.com>
2015-03-29 18:05:48 -0400

    added database settings

--- community/community/settings.py
+++ community/community/settings.py
@@ -58,8 +58,11 @@ WSGI_APPLICATION = 'community.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql.psycopg2',
+        'NAME': 'community',
+        'USER': 'server',
+        'PASSWORD': 'somethingstupidfornow',
+        'HOST':''
     }
 }
 
