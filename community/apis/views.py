32766d2a638e7f6e0e3eb2ac02d256208cb54743
Nathaniel Biggs <biggs0125@gmail.com>
2015-03-29 20:11:22 -0400

    Resolve conflict

--- community/apis/views.py
+++ community/apis/views.py
@@ -2,25 +2,25 @@ from django.shortcuts import render
 from django.views.generic import View
 from events.models import Event
 from datetime import datetime, time, date
-
+from django.http import HttpResponse
 class CreateEvent(View):
 
     def dispatch(self, request, *args, **kwargs):
 
-        if not request['method'] == 'POST':
-            pass
+        if not request.method == 'POST':
+            return HttpResponse(status=403)
 
-        self.name = request['name']
-        self.date = request['date']
-        self.time = request['time']
-        self.location = request['location']
-        self.description = request['description']
+        self.name = request.POST['name']
+        self.date = request.POST['date']
+        self.time = request.POST['time']
+        self.location = request.POST['location']
+        self.description = request.POST['description']
         
-        date_obj = date.strptime(self.date, '%m-%d-%Y')
-        time_obj = time.strptime(self.time, '%H:%M')
-        datetime_obj = datetime.combine(date_obj, time_obj)
+        date_obj = datetime.strptime(self.date, '%m-%d-%Y')
+        time_obj = datetime.strptime(self.time, '%H:%M')
+        datetime_obj = datetime.combine(date_obj.date(), time_obj.time())
         event = Event(name=self.name, datetime=datetime_obj, description=self.description,
                 location=self.location)
 
         event.save()
-
+        return HttpResponse(status=200)
--- community/community/settings.py
+++ community/community/settings.py
@@ -36,14 +36,19 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events',
+    'apis',
+    'organizations',
+    'tags',
+    'users'
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+#    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+#    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
