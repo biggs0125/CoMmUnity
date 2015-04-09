441af05233412c60b0b35a2eaa0ed945c770c069
Omar Rahman <omarr@andrew.cmu.edu>
2015-04-08 22:08:42 -0400

    Added creation and retrieval of events by tag(s)

--- community/apis/views.py
+++ community/apis/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from events.models import Event
+from tags.models import Tag
 from datetime import datetime, time, date
 from django.http import HttpResponse
 from django.core import serializers
@@ -17,14 +18,24 @@ class CreateEvent(View):
         self.time = request.POST['time']
         self.location = request.POST['location']
         self.description = request.POST['description']
+        self.tags = request.POST.getlist('tag')
         
-        date_obj = datetime.strptime(self.date, '%m-%d-%Y')
-        time_obj = datetime.strptime(self.time, '%H:%M')
+        try:
+            date_obj = datetime.strptime(self.date, '%Y-%m-%d')
+            time_obj = datetime.strptime(self.time, '%H:%M')
+        except ValueError:
+            return HttpResponse(status=400)
+
+
+        tags_list = [Tag.objects.get_or_create(name=tag)[0] for tag in self.tags]
+
         datetime_obj = datetime.combine(date_obj.date(), time_obj.time())
         event = Event(name=self.name, datetime=datetime_obj, description=self.description,
                 location=self.location)
 
+
         event.save()
+        event.tags = tags_list
         return HttpResponse(status=200)
 
 class GetEvent(View):
@@ -34,12 +45,15 @@ class GetEvent(View):
         return [Event.objects.get(pk=event_id)]
 
     def handle_date_range(self, start_date, end_date):
-        start_date = datetime.strptime(start_date, '%m-%d-%Y')
-        end_date = datetime.strptime(end_date+" 23:59:59", '%m-%d-%Y %H:%M:%S')
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        end_date = datetime.strptime(end_date+" 23:59:59", '%Y-%m-%d %H:%M:%S')
         return Event.objects.filter(datetime__gte=start_date, datetime__lte=end_date)
 
     def handle_date(self, event_date):
         return self.handle_date_range(event_date, event_date)
+
+    def handle_tags(self, tags):
+        return Event.objects.filter(tags__name__in=tags).distinct()
 
     def handle_params(self, qdict):
 
@@ -57,6 +71,9 @@ class GetEvent(View):
                 return self.handle_date_range(qdict['start_date'], qdict['end_date'])
             except ValueError:
                 return None
+
+        if 'tag' in qdict:
+            return self.handle_tags(qdict.getlist('tag'))
 
         return None
 
--- community/events/models.py
+++ community/events/models.py
@@ -7,8 +7,8 @@ class Event(models.Model):
 
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
-    attendees = models.ManyToManyField(User, null=True)
-    hosts = models.ManyToManyField(Organization, null=True)
-    tags = models.ManyToManyField(Tag, null=True)
+    attendees = models.ManyToManyField(User)
+    hosts = models.ManyToManyField(Organization)
+    tags = models.ManyToManyField(Tag)
     datetime = models.DateTimeField()
     location = models.CharField(max_length=60)
