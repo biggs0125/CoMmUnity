d8d143d989887cdda2fb755f8bb967c2e1964a4c
Kais Kudrolli <kkudrolli@gmail.com>
2015-04-12 17:31:44 -0400

    Added hrefs to 2 side bar buttons in base.html, added a submit button to create_event.html

--- community/events/js/create.js
+++ community/events/js/create.js
@@ -1,1 +1,23 @@
-var xmlhttp = new XMLHttpRequest();
+function create_event() {
+  
+  $.ajax({
+    url: "localhost:8000/api/event/create",
+    method: "POST",
+    data: {
+      name: $("#event-name").text(),
+      location: $("#location").text(),
+      description: $("#description").text(),
+      date: $("#dtpicker").text(),
+      time: $("#dtpicker").text(), // uhh..gotta changed this
+      tag: $("tags").value(),
+      organization: #("organization").value()
+    },
+    error: function (){
+             alert("Congrats you failed!");
+           },
+    success: function () {
+               alert("Ermahgerd I did it!");
+             }
+  });
+
+}
