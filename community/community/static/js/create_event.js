aab29c0bb689c9514664140809f6d4d979ef436d
Kais Kudrolli <kkudrolli@gmail.com>
2015-04-20 21:56:26 -0400

    Fixed date and time formatting using moment

--- community/community/static/js/create_event.js
+++ community/community/static/js/create_event.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   $("#submit").click(function() {
-    alert("Hello!"); 
     console.log('name:' + $("#event-name").val());
+    console.log('date:' + $("#dtpicker input").val().split(' ')[0]);
+    console.log('time:' + $("#dtpicker input").val().split(' ')[1]);
+
+    var dt = moment($("#dtpicker input").val()).format('YYYY-MM-DD HH:mm');
+
     $.ajax({
       url: "http://localhost:8000/api/event/create",
       method: "POST",
@@ -9,16 +13,18 @@ $(document).ready(function() {
         name: $("#event-name").val(),
       location: $("#location").val(),
       description: $("#description").val(),
-      date: $("#dtpicker").val().split(' ')[0],
-      time: $("#dtpicker").val().split(' ')[1], 
-      tag: $("#tags").val(),
+      //date: moment($("#dtpicker input").val().split(' ')[0]).format('YYYY-MM-DD'),
+      //time: moment($("#dtpicker input").val().split(' ')[1]).format('HH:mm'), 
+      date: dt.split(' ')[0],
+      time: dt.split(' ')[1], 
+      //tag: $("#tags").val(),
       //organization: $("#organization").val()
       },
       error: function (){
-               alert("Congrats you failed!");
+               console.log("Congrats you failed!");
              },
       success: function () {
-                 alert("Ermahgerd I did it!");
+                 console.log("Ermahgerd I did it!");
                }
     });
 
