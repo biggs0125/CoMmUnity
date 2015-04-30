ab7544629eaed159ea432a30e4226edc152b6dae
arieltian <arieltian99@gmail.com>
2015-04-30 17:25:03 -0400

    style the home page

--- community/home/static/home/home.js
+++ community/home/static/home/home.js
@@ -6,7 +6,17 @@ $(document).ready(function() {
 	success: function(data) {
 	    for (var i=0; i < data.length; i++) {
 		var event = data[i]['fields'];
-		$("#events_attending").append("<li>"+event.name+"</li>");
+                var tags = '';
+                for(var j=0; j < events.tags.length-1; j++) {
+                    tags += events.tags[j]['fields']['name'] + ", ";
+                }
+                tags += events.tags[j]['fields']['name'];
+                var eventstuff = "<ul><li>Description: "+event.description+"</li><li>Start: "+
+                    moment(event.start_datetime).format("LL h:mm")+"</li><li>End: "+
+                    moment(event.end_datetime).format("LL h:mm")+"</li><li>Tags: "+
+                    tags + "</li></ul>"
+                var eventname = "<li><b>"+event.name+"</b>"+eventstuff+"</li>";
+		$("#events_attending").append(eventname);
 	    }
 	}
     });
